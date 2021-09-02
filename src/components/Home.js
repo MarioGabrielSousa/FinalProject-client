dec437817c7b87fe36107d64f0537a03b859d84b
Mario Sousa <vegetalizado@gmail.com>
2021-09-02 07:11:03 +0100

    Revert "some login css"
    This reverts commit 6aa2dd9dad651ed0a297ee9e9c14020450b5b13f.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,11 @@
 import React from "react";
-import "./Styles/home.css";
 
 function Home() {
   return (
     <div className="home-page">
-      <div className="left">
-        <img src="/images/home-img.png" alt="home-img" />
-      </div>
-      <div className="right">
-        <h1>DAILY DOSE</h1>
-        <p>DIGITAL NOTEBOOK FOR YOUR WORKOUT ROUTINES</p>
-      </div>
+      <h1>DAILY DOSE</h1>
+      <p>DIGITAL NOTEBOOK FOR YOUR WORKOUT ROUTINES</p>
+      <img src="/images/home-img.png" alt="home-img" />
     </div>
   );
 }
